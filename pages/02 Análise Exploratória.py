da79f8cdc0d3d5b2b8f76f79b139687563d0a6ad
WESLEY ESTEVÃO DOS SANTOS <102426671+wesleyesantos@users.noreply.github.com>
2024-09-21 12:27:43 -0300

    Update 02 Análise Exploratória.py

--- pages/02 Análise Exploratória.py
+++ pages/02 Análise Exploratória.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 
-st.set_page_config(layout = "wide")
+st.set_page_config(page_title="Datathon Análise Exploratória",  page_icon="💎",layout = "wide")
 st.markdown("""<style>[data-testid="stSidebar"] {background: linear-gradient(180deg, #00c6ff, #0072ff);padding: 20px;}.icon-container {display: flex; align-items: center; margin-bottom: 20px;}.icon-container a {margin-right: 15px;}</style>""", unsafe_allow_html=True)
 with st.sidebar:
     st.markdown('''<font face= Helvetica><b><u>Desenvolvedor</u></b></font>''', unsafe_allow_html=True)
