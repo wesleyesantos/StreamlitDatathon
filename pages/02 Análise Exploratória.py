753fe918c1a3080b8eb8def6fcc3e7ca338a033c
WESLEY ESTEVÃO DOS SANTOS <102426671+wesleyesantos@users.noreply.github.com>
2025-08-30 13:03:45 -0300

    Update 02 Análise Exploratória.py

--- pages/02 Análise Exploratória.py
+++ pages/02 Análise Exploratória.py
@@ -18,7 +18,7 @@ with st.sidebar:
     <a href="{youtube_url}" target="_blank"><img src="https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/youtube.png?raw=true" width="30"/></a>
     <a href="{github_url}" target="_blank"><img src="https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/github.png?raw=true" width="30"/></a>
     </div>""", unsafe_allow_html=True)
-    st.sidebar.image('https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/eu.jpg?raw=true', use_column_width=True)
+    st.sidebar.image('https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/eu.jpg?raw=true')
 
 url = "https://github.com/wesleyesantos/Postech-Datathon/raw/main/PEDE_PASSOS_DATASET_FIAP.csv"
 url1 = "https://github.com/4ca63473-734d-4d8c-8181-9635c1837ddc"
