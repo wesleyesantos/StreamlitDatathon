a0d9dd172649aa95eb1f3112d0d20d6093671c25
WESLEY ESTEVÃO DOS SANTOS <102426671+wesleyesantos@users.noreply.github.com>
2025-08-30 13:01:51 -0300

    Update Início.py

--- Início.py
+++ Início.py
@@ -17,7 +17,7 @@ with st.sidebar:
     <a href="{youtube_url}" target="_blank"><img src="https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/youtube.png?raw=true" width="30"/></a>
     <a href="{github_url}" target="_blank"><img src="https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/github.png?raw=true" width="30"/></a>
     </div>""", unsafe_allow_html=True)
-    st.sidebar.image('https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/eu.jpg?raw=true', use_column_width=True)
+    st.sidebar.image('https://github.com/wesleyesantos/StreamlitDatathon/blob/main/images/eu.jpg?raw=true')
 
 
 st.title(":blue[Evolução dos Alunos na PASSOS MÁGICOS [2020-2022]]")
